b621444f5dc36e11799de8f9467cca1a0da0ad05
connieliu0 <cliu55567@gmail.com>
2020-08-14 16:07:29 -0400

    updated translation files

---
+++ build/precache-manifest.0ee390f5bfae26bd548ca2553e141a86.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e15ad15aa4b0ce3f15523cabb7a60a3",
+    "url": "/p5.js-showcase/index.html"
+  },
+  {
+    "revision": "47fb672a23b7e5ac0f9d",
+    "url": "/p5.js-showcase/static/css/main.075995e6.chunk.css"
+  },
+  {
+    "revision": "32e4c6fe1530918e15d2",
+    "url": "/p5.js-showcase/static/js/2.f2a4c7e5.chunk.js"
+  },
+  {
+    "revision": "f727e6382ce1e9a0c6b9f0a495db85a8",
+    "url": "/p5.js-showcase/static/js/2.f2a4c7e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47fb672a23b7e5ac0f9d",
+    "url": "/p5.js-showcase/static/js/main.e8e2e82a.chunk.js"
+  },
+  {
+    "revision": "a9b65fb79d54eb48e6f1",
+    "url": "/p5.js-showcase/static/js/runtime-main.b3f5858e.js"
+  },
+  {
+    "revision": "d393e537ce681b118021fe36705ffd7f",
+    "url": "/p5.js-showcase/static/media/1.d393e537.jpg"
+  },
+  {
+    "revision": "a24731852c043af15f16c4077dc2e56f",
+    "url": "/p5.js-showcase/static/media/2 - Vince Nguyen.a2473185.jpg"
+  },
+  {
+    "revision": "e601fb1f61d478d85cef561587f5d642",
+    "url": "/p5.js-showcase/static/media/2.e601fb1f.jpg"
+  },
+  {
+    "revision": "baa15ee03f1692f8b6c5ecb02d215f9b",
+    "url": "/p5.js-showcase/static/media/3.baa15ee0.jpg"
+  },
+  {
+    "revision": "115cb70d55d781d70db4b8cde7620a8b",
+    "url": "/p5.js-showcase/static/media/36daysoftypexp5js - user name.115cb70d.jpg"
+  },
+  {
+    "revision": "8a13cfbd32be74eef07fd9709a75d974",
+    "url": "/p5.js-showcase/static/media/7 - Vince Nguyen.8a13cfbd.jpg"
+  },
+  {
+    "revision": "dc4b2366d489397556dfe723f90e565d",
+    "url": "/p5.js-showcase/static/media/DigitalMirror (3) - Hanif Haghtalab.dc4b2366.jpg"
+  },
+  {
+    "revision": "7f1b2ea4fa44ba97d6b83618a7e8b505",
+    "url": "/p5.js-showcase/static/media/DigitalMirror (4) - Hanif Haghtalab.7f1b2ea4.jpg"
+  },
+  {
+    "revision": "8693ce4b7efe6e23cf7d40225a5765cc",
+    "url": "/p5.js-showcase/static/media/ESB4DgjUYAABY1z - Panutat Tejasen.8693ce4b.jpg"
+  },
+  {
+    "revision": "20bde2264eb5cb94b30fd3e6440d5ce5",
+    "url": "/p5.js-showcase/static/media/GenerativeIdentity - Alaazym Codes.20bde226.jpg"
+  },
+  {
+    "revision": "2b1705203eca034a6fc328fa0ad7cdb6",
+    "url": "/p5.js-showcase/static/media/Memori - Sashrika Pandey.2b170520.jpg"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/p5.js-showcase/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/p5.js-showcase/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/p5.js-showcase/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/p5.js-showcase/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9f005d6ead1fb88d6e493ec9134a1f58",
+    "url": "/p5.js-showcase/static/media/airi-flies.9f005d6e.png"
+  },
+  {
+    "revision": "7e83996f462f1dc8c3395db1c5807eaa",
+    "url": "/p5.js-showcase/static/media/asterisk-01-01.7e83996f.png"
+  },
+  {
+    "revision": "b1dc80ed277b692c2b2283ee1621d1f3",
+    "url": "/p5.js-showcase/static/media/asterisk-01.b1dc80ed.svg"
+  },
+  {
+    "revision": "b22f96356782d78922ce26dd135a18d0",
+    "url": "/p5.js-showcase/static/media/chillin.b22f9635.png"
+  },
+  {
+    "revision": "f03fedef490e73982772e1948b103437",
+    "url": "/p5.js-showcase/static/media/moving-responsive-posters.f03fedef.png"
+  },
+  {
+    "revision": "c59c4ee955c993b00b8f64e26c9a44b0",
+    "url": "/p5.js-showcase/static/media/p5-shaders.c59c4ee9.png"
+  },
+  {
+    "revision": "ecb2d9b8f13b107da124a956018025fd",
+    "url": "/p5.js-showcase/static/media/programmed-plotter.ecb2d9b8.png"
+  },
+  {
+    "revision": "12ca14aa44c490342eee7899c169b891",
+    "url": "/p5.js-showcase/static/media/qtv.12ca14aa.png"
+  },
+  {
+    "revision": "781234d0a2723d0c835f075f6ed82152",
+    "url": "/p5.js-showcase/static/media/squigglepatterngenerator - Jennifer Kowalski.781234d0.jpg"
+  },
+  {
+    "revision": "b5460f9d7b649acf1b0ffa7d3d0694d6",
+    "url": "/p5.js-showcase/static/media/theseasonschange.fileext - Neha Malepati.b5460f9d.jpg"
+  },
+  {
+    "revision": "f1008fc5fe19c8d613ed176fc5efff11",
+    "url": "/p5.js-showcase/static/media/whatsupinthegut - Olga Khvan.f1008fc5.jpg"
+  },
+  {
+    "revision": "9c8821a40061fde2d533740b5aa0c097",
+    "url": "/p5.js-showcase/static/media/スクリーンショット 2020-07-08 23.32.06 - Kosuke Murakami.9c8821a4.jpg"
+  }
+]);
